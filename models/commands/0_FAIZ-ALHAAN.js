d978da0e3a9f682bb1ad832a0b2cd13791c99a6f
Ansarihu <zahruddina54@gmail.com>
2025-02-27 21:18:07 +0530

    Update commands

---
+++ models/commands/0_FAIZ-ALHAAN.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "Alhaan",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "FAIZ ANSARI", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "alhaan",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("@Alhaan Khan")==0 || event.body.indexOf("ALHAAN")==0 || event.body.indexOf("alhaan")==0 || event.body.indexOf("Alhaan khan")==0) {
+    var msg = {
+        body: "𝐘𝐄 𝐃𝐄𝐊𝐇𝐎 𝑺𝑨𝑵𝑨𝑨 𝐉𝐈 𝐀 𝐆𝐀𝐘𝐈",
+        attachment: fs.createReadStream(__dirname + `/FAIZ/SANA.webp`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
