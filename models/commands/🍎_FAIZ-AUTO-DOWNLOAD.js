88eebed96092b0a4faa6ae0547c63fbe64d8d6f0
Ansarihu <zahruddina54@gmail.com>
2025-03-03 05:14:47 +0530

    Update 🍎_FAIZ-AUTO-DOWNLOAD.js

--- models/commands/🍎_FAIZ-AUTO-DOWNLOAD.js
+++ models/commands/🍎_FAIZ-AUTO-DOWNLOAD.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "linkAutoDownload",
     version: "1.3.0",
     hasPermssion: 0,
-    credits: "FAIZ ANSARI",
+    credits: "FAIZ BABU",
     description:
       "Automatically detects links in messages and downloads the file.",
     commandCategory: "Utilities",
@@ -17,13 +17,13 @@ module.exports = {
     const fs = require("fs-extra");
     const content = event.body ? event.body : "";
     const body = content.toLowerCase();
-    const { alldown } = require("prem-babu-media");
+    const { alldown } = require("arif-babu-media");
     if (body.startsWith("https://")) {
-      api.setMessageReaction("📿", event.messageID, (err) => {}, true);
+      api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
       const data = await alldown(content);
       console.log(data);
       const { low, high, title } = data.data;
-      api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true);
+      api.setMessageReaction("📥", event.messageID, (err) => {}, true);
       const video = (
         await axios.get(high, {
           responseType: "arraybuffer",
@@ -36,7 +36,7 @@ module.exports = {
 
       return api.sendMessage(
         {
-          body: `⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆\n\nᴛɪᴛʟᴇ: ${title}\n\n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`,
+          body: `❥━━━━❥[ 😀 ]❥━━━━❥\n\nᴛɪᴛʟᴇ: ${title}\n\n❥━━━━❥[ 😈 ]❥━━━━❥`,
           attachment: fs.createReadStream(__dirname + "/cache/auto.mp4"),
         },
         event.threadID,
